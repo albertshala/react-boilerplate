c98af0fb2c93ae1f518f3e4d399f025e3c15ff85
Albert Shala <albert_shala@trendmicro.com>
2020-06-11 14:29:14 -0400

    Configuring Amplify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 import App from './app';
 import './styles.css';
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(<App />, document.getElementById('App'));
